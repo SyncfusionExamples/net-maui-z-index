0a50a38ab68b4cd38ccbd3bb33ec7905c0ba23c2
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2022-04-21 17:15:40 +0530

    Z-Index on .NET MAUI - Blog sample added.

---
+++ NetMauiZIndex/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NetMauiZIndex;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ NetMauiZIndex/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace NetMauiZIndex
+{
+    public class ViewModel
+    {
+        private static int Zindex = 10;
+        private Command bringToFront;
+
+        public Command BringToFront
+        {
+            get { return bringToFront; }
+            set { bringToFront = value; }
+        }
+        public ViewModel()
+        {
+            bringToFront = new Command<object>(OnBringToFrontAsync);            
+        }
+
+        private async void OnBringToFrontAsync(object view)
+        {
+            var viewName = (view as Label).Text;
+            bool answer = await Application.Current.MainPage.DisplayAlert("Bring to front", "I am "+viewName + " , " + "Shall i move to front ?", "Yes", "No");
+            if (answer)
+            {
+                (view as Label).ZIndex = Zindex;
+                Zindex += 1;
+            }
+        }
+    }
+}
